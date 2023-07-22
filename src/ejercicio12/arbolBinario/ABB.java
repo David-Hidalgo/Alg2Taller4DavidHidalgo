7d2cb7b99c24b66beb5e5f12d26eb25c191984fa
David Eduardo Hidalgo Varela <dehidalgo.22@est.ucab.edu.ve>
2023-07-22 13:28:20 -0400

    update ABB.class, Nodo.class, ABB.java and Nodo.java

--- src/ejercicio12/arbolBinario/ABB.java
+++ src/ejercicio12/arbolBinario/ABB.java
@@ -1,24 +1,50 @@
+/**
+ * Clase que representa un árbol binario de búsqueda (ABB).
+ * Un ABB es un tipo de árbol binario en el que para cada nodo, 
+ * todos los nodos del subárbol izquierdo tienen un valor menor 
+ * que el nodo y todos los nodos del subárbol derecho tienen un 
+ * valor mayor que el nodo.
+ */
 package ejercicio12.arbolBinario;
 
 public class ABB {
     protected Nodo raiz;
 
+    /**
+     * Constructor por defecto que inicializa la raíz del árbol como nula.
+     */
     public ABB() {
         raiz = null;
     }
 
+    /**
+     * Constructor que inicializa la raíz del árbol con un valor dado.
+     * @param valor El valor a asignar a la raíz del árbol.
+     */
     public ABB(int valor) {
         raiz = new Nodo(valor);
     }
 
+    /**
+     * Establece la raíz del árbol.
+     * @param raiz La nueva raíz del árbol.
+     */
     public void setRaiz(Nodo raiz) {
         this.raiz = raiz;
     }
 
+    /**
+     * Obtiene la raíz del árbol.
+     * @return La raíz del árbol.
+     */
     public Nodo getRaiz() {
         return raiz;
     }
 
+    /**
+     * Inserta un nuevo nodo con un valor dado en el árbol.
+     * @param valorInsertar El valor a insertar en el árbol.
+     */
     public void insertar(int valorInsertar) {
         if (this.raiz == null) {
             this.raiz = new Nodo(valorInsertar);
@@ -26,6 +52,13 @@ public class ABB {
             this.raiz = insertar(valorInsertar, this.raiz);
         }
     }
+
+    /**
+     * Método privado que inserta un nuevo nodo con un valor dado en el árbol.
+     * @param valorInsertar El valor a insertar en el árbol.
+     * @param nodo El nodo actual en el que se está insertando el valor.
+     * @return El nodo actualizado con el nuevo valor insertado.
+     */
     private Nodo insertar(int valorInsertar, Nodo nodo) {
         if (nodo.getClave() == 0) {
             nodo.setClave(valorInsertar);
@@ -46,20 +79,31 @@ public class ABB {
         }
         return nodo;
     }
+
+    /**
+     * Recorre el árbol en orden y muestra los valores de los nodos.
+     */
     public void inOrden() {
         if (raiz != null) {
             raiz.inOrden();
         }
     }
+
+    /**
+     * Recorre el árbol en preorden y muestra los valores de los nodos.
+     */
     public void preOrden() {
         if (raiz != null) {
             raiz.preOrden();
         }
     }
+
+    /**
+     * Recorre el árbol en postorden y muestra los valores de los nodos.
+     */
     public void postOrden() {
         if (raiz != null) {
             raiz.postOrden();
         }
     }
-
 }
--- src/ejercicio12/arbolBinario/Nodo.java
+++ src/ejercicio12/arbolBinario/Nodo.java
@@ -1,3 +1,6 @@
+/**
+ * Esta clase representa un nodo en un árbol binario. Contiene un valor de clave, un nodo hijo izquierdo y un nodo hijo derecho.
+ */
 package ejercicio12.arbolBinario;
 
 public class Nodo {
@@ -5,40 +8,76 @@ public class Nodo {
     private Nodo hijoIzquierdo;
     private Nodo hijoDerecho;
 
+    /**
+     * Constructor para un nodo con un valor de clave dado.
+     * @param clave el valor de clave para el nodo.
+     */
     public Nodo(int clave) {
         this.clave = clave;
         hijoIzquierdo = hijoDerecho = null;
     }
 
+    /**
+     * Constructor predeterminado para un nodo con un valor de clave de 0.
+     */
     public Nodo() {
         this.clave = 0;
         hijoIzquierdo = hijoDerecho = null;
     }
 
+    /**
+     * Método getter para el valor de clave del nodo.
+     * @return el valor de clave del nodo.
+     */
     public int getClave() {
         return clave;
     }
 
+    /**
+     * Método setter para el valor de clave del nodo.
+     * @param dato el nuevo valor de clave para el nodo.
+     */
     public void setClave(int dato) {
         this.clave = dato;
     }
 
+    /**
+     * Método getter para el nodo hijo derecho.
+     * @return el nodo hijo derecho.
+     */
     public Nodo getHijoDerecho() {
         return hijoDerecho;
     }
 
+    /**
+     * Método setter para el nodo hijo derecho.
+     * @param hijoDerecho el nuevo nodo hijo derecho.
+     */
     public void setHijoDerecho(Nodo hijoDerecho) {
         this.hijoDerecho = hijoDerecho;
     }
 
+    /**
+     * Método getter para el nodo hijo izquierdo.
+     * @return el nodo hijo izquierdo.
+     */
     public Nodo getHijoIzquierdo() {
         return hijoIzquierdo;
     }
 
+    /**
+     * Método setter para el nodo hijo izquierdo.
+     * @param hijoIzquierdo el nuevo nodo hijo izquierdo.
+     */
     public void setHijoIzquierdo(Nodo hijoIzquierdo) {
         this.hijoIzquierdo = hijoIzquierdo;
     }
 
+    /**
+     * Calcula la altura del nodo.
+     * @param nodo el nodo para calcular la altura.
+     * @return la altura del nodo.
+     */
     public int altura(Nodo nodo) {
         if (nodo == null) {
             return 0;
@@ -47,6 +86,11 @@ public class Nodo {
         }
     }
 
+    /**
+     * Busca un nodo con un valor de clave dado.
+     * @param valorABuscar el valor de clave a buscar.
+     * @return el nodo con el valor de clave dado, o null si no se encuentra.
+     */
     public Nodo buscarNodo(int valorABuscar) {
         Nodo aux;
         if (this.clave == valorABuscar)
@@ -67,6 +111,9 @@ public class Nodo {
         return aux;
     }
 
+    /**
+     * Imprime los nodos del árbol en recorrido in-order.
+     */
     public void inOrden() {
         if (hijoIzquierdo != null) {
             hijoIzquierdo.inOrden();
@@ -77,6 +124,9 @@ public class Nodo {
         }
     }
 
+    /**
+     * Imprime los nodos del árbol en recorrido pre-order.
+     */
     public void preOrden() {
         System.out.println(clave);
         if (hijoIzquierdo != null) {
@@ -87,6 +137,9 @@ public class Nodo {
         }
     }
 
+    /**
+     * Imprime los nodos del árbol en recorrido post-order.
+     */
     public void postOrden() {
         if (hijoIzquierdo != null) {
             hijoIzquierdo.postOrden();
